c4eda8c04703bfae076bf47e16997be9eab9dfc1
HsekumRamuk <mukeshfont@gmail.com>
2024-06-15 17:11:43 +0530

    Protecting against Nan error

--- Oscilator_All_Direction.cs
+++ Oscilator_All_Direction.cs
@@ -25,6 +25,8 @@ public class Oscilator_All_Direction : MonoBehaviour
     void Update()
     {
 
+        if (speed <= Mathf.Epsilon)     // protecting against Nan error
+        {return;}
         float cycles = Time.time / speed;       // defining speed of the object
         float sinWave = MathF.Sin(cycles);      // generating the value from -1 and 0 using Sin function
         float movementFactor = (sinWave + 1) / 2;   // limit the value from 0 to 1
